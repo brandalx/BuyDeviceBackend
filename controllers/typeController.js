67c63edcb1bb5aa5d2ea00bd1a770ac486b37b2c
brndalx <brndalx@gmail.com>
2023-04-28 13:47:37 +0300

    feat: types controller post and get all functions

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -7,7 +7,10 @@ class TypeController {
     const type = await Type.create({ name });
     return res.json(type);
   }
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    const types = await Type.findAll();
+    return res.json(types);
+  }
 }
 
 module.exports = new TypeController();
