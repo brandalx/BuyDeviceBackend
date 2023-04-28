68cb6e2a977f5820fd22b46072318c44789622fd
brndalx <brndalx@gmail.com>
2023-04-28 13:07:10 +0300

    feat: controllers for device brand and type

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
 class UserController {
   async registration(req, res) {}
   async login(req, res) {}
-  async check(req, res) {}
+  async check(req, res) {
+    res.json("sssssss");
+  }
 }
 
 module.exports = new UserController();
