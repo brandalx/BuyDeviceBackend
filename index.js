54cd55c36275bd5d26329366cc14a3c167e6bf7f
brndalx <brndalx@gmail.com>
2023-04-28 12:57:12 +0300

    feat: routers basic initialization

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models.js");
 const cors = require("cors");
+const router = require("./routes/index");
 const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api", router);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is working correctly" });
+});
+app.get("/*", (req, res) => {
+  res.json({ message: "404 not found" });
 });
 const start = async () => {
   try {
