34825a39659c9c5ba688975bb17d181874745c53
brndalx <brndalx@gmail.com>
2023-04-28 13:23:25 +0300

    fix: middleware export, feat: error works correct

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,15 @@
+const ApiError = require("../errors/apiErrors");
+
 class UserController {
   async registration(req, res) {}
   async login(req, res) {}
-  async check(req, res) {
-    res.json("sssssss");
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      next(ApiError.badRequest("No id were provided"));
+    }
+
+    res.json(id);
   }
 }
 
--- errors/apiErrors.js
+++ errors/apiErrors.js
@@ -14,3 +14,4 @@ class ApiError extends Error {
     return new ApiError(403, message);
   }
 }
+module.exports = ApiError;
