dcfefb5e5f48ee23828d53615e4a85e3e155a9d1
brndalx <brndalx@gmail.com>
2023-04-28 19:29:16 +0300

    feat: middleware for checking role for type router

--- routes/typeRouter.js
+++ routes/typeRouter.js
@@ -1,9 +1,9 @@
 const Router = require("express");
 const router = new Router();
 const typeController = require("../controllers/typeController");
-const { getAll } = require("../controllers/brandController");
-const { type } = require("os");
-router.post("/", typeController.create);
+const checkRole = require("../middleware/checkRoleMiddleware");
+
+router.post("/", checkRole("ADMIN"), typeController.create);
 router.get("/", typeController.getAll);
 
 module.exports = router;
